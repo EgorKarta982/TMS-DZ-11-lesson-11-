dbfe543ed5d003601244ad2e04d3e687bb53696e
Egor Kartshev <egorkarta982@gmail.com>
2022-06-30 18:47:30 +0300

    animation, timer

--- TMS DZ 11 (lesson 11)/ViewController.swift
+++ TMS DZ 11 (lesson 11)/ViewController.swift
@@ -1,19 +1,44 @@
-//
-//  ViewController.swift
-//  TMS DZ 11 (lesson 11)
-//
-//  Created by Егор on 30.06.2022.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    
+    let myView = UIView(frame: CGRect(x: 0, y: 500, width: 100, height: 100))
+    var isDown = false
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myView.backgroundColor = .darkGray
+        self.view.addSubview(myView)
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let image = UIImage(named: "Foto")
+        self.imageView.image = image
+    }
+    
+    @IBAction func animationGoButtonPressed(_ sender: UIButton) {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.moveView()
+        }
+        timer.fire() //запуск таймера без задержки
+        
+    }
+    
+    private func moveView(){
+        let position = isDown == true ? self.myView.frame.origin.y - 200 : self.myView.frame.origin.y + 200
+        self.isDown = !self.isDown
+        
+        UIView.animate(withDuration: 0.3) {
+            self.myView.frame.origin.y = position
+        }
+    }
+    
+    @IBAction func stopTimerButtonPressed(_ sender: UIButton) {
+        self.timer.invalidate()
+    }
 }
-
